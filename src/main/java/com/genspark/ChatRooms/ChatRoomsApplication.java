e397f731a22c65a7a9e2a0778f36fa628e5a7d63
rayalva407 <rayalva407@gmail.com>
2022-06-16 13:09:21 -0600

    Configures WebSocket

---
+++ src/main/java/com/genspark/ChatRooms/ChatRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.ChatRooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genspark/ChatRooms/ChatRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.ChatRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
