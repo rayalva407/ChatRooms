162b3f11df6781f799ee54d86c2f7204e44d69a7
rayalva407 <rayalva407@gmail.com>
2022-06-17 11:22:22 -0600

    Configures the ChatController

--- src/main/java/com/genspark/ChatRooms/Controller/ChatController.java
+++ src/main/java/com/genspark/ChatRooms/Controller/ChatController.java
@@ -1,8 +1,30 @@
 package com.genspark.ChatRooms.Controller;
 
+import com.genspark.ChatRooms.Model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    // We want the user to send messages through this method with this mapping
+    // When this method receives message in this particular mapping then it needs to be sent to /chatroom/public
+    @MessageMapping("/message") // /app/message
+    @SendTo("/chatroom/public")
+    public Message receiveMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
+        return message;
+    }
 }
